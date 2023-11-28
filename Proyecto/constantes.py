ad74e1e1373d16b921d4e2e9b638aa2731555f29
Patricio Benavides <patojavier1981@gmail.com>
2023-11-27 21:39:40 -0300

    prueba funcion cargar imagen

--- Proyecto/constantes.py
+++ Proyecto/constantes.py
@@ -1,4 +1,4 @@
-
+import pygame as PY
 
 pW = 800
 pH = 800
@@ -10,3 +10,29 @@ cW = pH // nyC
 min_cW = pW // nxC
 min_cH = pH // nyC
 velocidad_movimiento = 20
+
+
+""" def cargarimagen(nombre):
+    return PY.transform.scale(PY.image.load(nombre), (cW, cH))
+"""
+
+""" leonImg = cargarimagen("Proyecto/imagenes/leon.png")
+guepardoImg = cargarimagen("Proyecto/imagenes/guepardo.png")
+loboImg = cargarimagen("Proyecto/imagenes/lobo.png")
+osoImg = cargarimagen("Proyecto/imagenes/oso.png")
+zorroImg = cargarimagen("Proyecto/imagenes/zorro.png")
+
+conejoImg = cargarimagen("Proyecto/imagenes/conejo.png")
+vacaImg = cargarimagen("Proyecto/imagenes/vaca.png")
+ovejaImg = cargarimagen("Proyecto/imagenes/oveja.png")
+cerdoImg = cargarimagen("Proyecto/imagenes/cerdo.png")
+conejoImg = cargarimagen("Proyecto/imagenes/conejo.png")
+
+nenufarImg = cargarimagen("Proyecto/imagenes/nenufar.png")
+arbolDesiertoImg = cargarimagen("Proyecto/imagenes/arbolDesierto.png")
+arbolTierraImg = cargarimagen("Proyecto/imagenes/arbolTierra.png")
+
+tierraImg = cargarimagen("Proyecto/imagenes/tierra.png")
+desiertoImg = cargarimagen("Proyecto/imagenes/desierto.png")
+aguaImg = cargarimagen("Proyecto/imagenes/agua.png")
+ """
